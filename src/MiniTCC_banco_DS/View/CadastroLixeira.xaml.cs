a4c9b50745426a4ec6937e3ce1b03d35f190597d
brunoyugue <brunoyuguecardoso@gmail.com>
2026-09-09 11:19:31 -0300

    Limpeza da estrutura do git

---
+++ src/MiniTCC_banco_DS/View/CadastroLixeira.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiniTCC_banco_DS.View
+{
+    /// <summary>
+    /// Lógica interna para CadastroLixeira.xaml
+    /// </summary>
+    public partial class CadastroLixeira : Window
+    {
+        public CadastroLixeira()
+        {
+            InitializeComponent();
+        }
+    }
+}
