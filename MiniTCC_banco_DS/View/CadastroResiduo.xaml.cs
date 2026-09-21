a5ea6a40970c35e914020d3cdd80020e86f582c7
Enzo <enzobassomartins@gmail.com>
2026-09-21 06:47:38 -0300

    Add files via upload

---
+++ MiniTCC_banco_DS/View/CadastroResiduo.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiniTCC_banco_DS.View
+{
+    /// <summary>
+    /// Lógica interna para CadastroResiduo.xaml
+    /// </summary>
+    public partial class CadastroResiduo : Window
+    {
+        public CadastroResiduo()
+        {
+            InitializeComponent();
+        }
+   
+
+        private void BtnVoltar_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ MiniTCC_banco_DS/View/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiniTCC_banco_DS.View
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            if (Opcoes.Visibility == Visibility.Collapsed)
+            {
+                Opcoes.Visibility = Visibility.Visible;
+                BtnIniciar.Content = "Fechar";
+            }
+            else
+            {
+                Opcoes.Visibility = Visibility.Collapsed;
+                BtnIniciar.Content = "Iniciar";
+            }
+        }
+
+        private void BtnCadastroLixeira_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroLixeira cadastroLixeira = new CadastroLixeira(); //mudar de pagina
+            cadastroLixeira.Show();
+            this.Close();
+        }
+
+        private void BtnCadastroResiduos_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroResiduo cadastroResiduos = new CadastroResiduo();
+            cadastroResiduos.Show();
+            this.Close();
+        }
+
+        private void BtnCadastroRegistros_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroRegistro cadastroRegistros = new CadastroRegistro();
+            cadastroRegistros.Show();
+            this.Close();
+        }
+    }
+
+}
